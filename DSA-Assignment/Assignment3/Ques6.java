d929b821d0987bca62a353f34847d373b0c33584
knpravirawat111 <knpravirawat111@gmail.com>
2023-07-08 18:01:25 +0530

    add question 6

---
+++ DSA-Assignment/Assignment3/Ques6.java
@@ -0,0 +1,13 @@
+public class Ques6 {
+    public static int singleNumber(int[] nums) {
+        int ans=0;
+        for(int i=0;i<nums.length;i++){
+         ans=ans^nums[i];
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int arr []={2,2,1};
+        System.out.println(singleNumber(arr));
+    }
+}
